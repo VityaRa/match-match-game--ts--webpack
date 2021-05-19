c5e1d5dcb1c72558c3631ac8a4377547e347b44c
VityaRa <blackops2in@mail.ru>
2021-05-19 23:14:26 +0300

    feat: add methods clear and addCards for CardsField

--- src/components/card/card.ts
+++ src/components/card/card.ts
@@ -1,17 +1,26 @@
 import { BaseComponent } from '../base-component';
 import './card.scss';
 
+const FLIP_CLASS = 'flipped';
 type imageURL = string;
 
 export class Card extends BaseComponent {
-  constructor(image: imageURL) {
+  constructor(readonly image: imageURL) {
     super('div', ['card-container']);
 
     this.element.innerHTML = `
-    <div class="card">
-      <div class="card__front" style="background-image: url('./images/${image}');"></div>
-      <div class="card__back"></div>
-    </div>
+      <div class="card">
+        <div class="card__front" style="background-image: url('./images/${image}');"></div>
+        <div class="card__back"></div>
+      </div>
     `;
+  }
+
+  flipOut(): void {
+    this.element.classList.remove(FLIP_CLASS);
+  }
+
+  flipTo(): void {
+    this.element.classList.add(FLIP_CLASS);
   }
 }
--- src/components/cards-field/cards-field.ts
+++ src/components/cards-field/cards-field.ts
@@ -1,10 +1,21 @@
-import { BaseComponent } from "../base-component";
-import './cards-field.scss'
+import { BaseComponent } from '../base-component';
+import { Card } from '../card/card';
+import './cards-field.scss';
 
 export class CardsField extends BaseComponent {
+  private cards: Card[] = [];
+
   constructor() {
-    super('div', ["cards-field"])
+    super('div', ['cards-field']);
   }
 
+  clear(): void {
+    this.cards = [];
+    this.element.innerHTML = '';
+  }
 
+  addCards(cards: Card[]): void {
+    this.cards = cards;
+    this.cards.forEach(card => this.element.appendChild(card.element))
+  }
 }
