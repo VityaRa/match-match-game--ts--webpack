7dc4e03a92a9fd49ba3f30c03a143ba54935d545
VityaRa <blackops2in@mail.ru>
2021-05-20 01:56:36 +0300

    fix: fix selector for app div

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,9 @@ import img from './assets/card-image.png'
 import { App } from './app'
 
 window.onload = () => {
-  const $app = document.getElementById('#app')
+  const $app = document.getElementById('app')
 
   if (!$app) throw Error('App root not found')
 
-  new App($app)
+  new App($app).start()
 }
