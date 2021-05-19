092492b78ef635b5243a6778e708082fe696ad96
VityaRa <blackops2in@mail.ru>
2021-05-20 01:55:17 +0300

    feat:: add async start method

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,21 @@
 import { CardsField } from "./components/cards-field/cards-field";
+import { Game } from "./components/game/game";
+import { ImageCategory } from './models/image-category'
 
 export class App {
-  private readonly cardsField: CardsField;
+  private readonly game: Game;
+
 
   constructor(private readonly rootElement: HTMLElement) {
-    this.cardsField = new CardsField()
-    this.rootElement.appendChild(this.cardsField.element)
+    this.game = new Game()
+    this.rootElement.appendChild(this.game.element)
+  }
+
+  async start() {
+    const res = await fetch('./images.json')
+    const data: ImageCategory[] = await res.json()
+    const categories = data[0]
+    const images = categories.images.map(name => `${categories.category}/${name}`)
+    this.game.startGame(images)
   }
 }
