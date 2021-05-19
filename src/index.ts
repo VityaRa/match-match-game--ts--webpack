c3ada0f46a32ce1353d7b68bf0c3a5604ebf0310
VityaRa <blackops2in@mail.ru>
2021-05-19 22:28:30 +0300

    feat: add main basic components

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,11 @@
 import './styles.scss';
 import img from './assets/card-image.png'
+import { App } from './app'
+
+window.onload = () => {
+  const $app = document.getElementById('#app')
+
+  if (!$app) throw Error('App root not found')
+
+  new App($app)
+}
